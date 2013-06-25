1c9fd98745402dd197ac2bd0c7778585c3539f81
torms3 <kisuklee@mit.edu>
2013-06-24 22:22:51 -0400

    First script: generate 3D affinity graph from 3D labels

---
+++ code/generate_affinity_graph.m
@@ -0,0 +1,55 @@
+function [G] = generate_affinity_graph( lbl )
+
+	% assert 3D
+	assert(numel(size(lbl)) == 3);
+
+
+	% B stands for boundary	
+	B = (lbl == 0);
+
+
+	%% x-axis
+	%
+	bbMask = B(:,1:end-1,:) & B(:,2:end,:);	% boundary-boundary affinity mask
+
+	x = lbl(:,1:end-1,:) - lbl(:,2:end,:);
+	bMap = (x ~= 0) | bbMask;	% boundary map
+
+	x(bMap) = 0;
+	x(~bMap) = 1;
+	x = x(1:end-1,:,1:end-1);
+
+
+	%% y-axis
+	%
+	% B stands for boundary
+	bbMask = B(1:end-1,:,:) & B(2:end,:,:);	% boundary-boundary affinity mask
+	
+	y = lbl(1:end-1,:,:) - lbl(2:end,:,:);
+	bMap = (y ~= 0) | bbMask;	% boundary map
+
+	y(bMap) = 0;
+	y(~bMap) = 1;
+	y = y(:,1:end-1,1:end-1);
+
+
+	%% z-axis
+	%
+	% B stands for boundary
+	bbMask = B(:,:,1:end-1) & B(:,:,2:end);	% boundary-boundary affinity mask
+	
+	z = lbl(:,:,1:end-1) - lbl(:,:,2:end);
+	bMap = (z ~= 0) | bbMask;	% boundary map
+
+	z(bMap) = 0;
+	z(~bMap) = 1;
+	z = z(1:end-1,1:end-1,:);
+
+
+	%% Affinity graph
+	%
+	G.x = x;
+	G.y = y;
+	G.z = z;
+
+end
