1b50907a03a02793945927dcdb0a2d91dbe0dd77
torms3 <kisuklee@mit.edu>
2015-01-10 15:19:49 -0500

    data -> result

--- code/VolumeAnalysis/assess_affinity_graph_script.m
+++ code/VolumeAnalysis/assess_affinity_graph_script.m
@@ -11,11 +11,11 @@ function assess_affinity_graph_script( fname, data, filtrad )
 	%% Voxel error
 	%
 	disp(['Processing x-affinity...']);
-	[data.x] = optimize_voxel_error(prep.P.x,prep.G.x);
+	[result.x] = optimize_voxel_error(prep.P.x,prep.G.x);
 	disp(['Processing y-affinity...']);
-	[data.y] = optimize_voxel_error(prep.P.y,prep.G.y);
+	[result.y] = optimize_voxel_error(prep.P.y,prep.G.y);
 	disp(['Processing z-affinity...']);
-	[data.z] = optimize_voxel_error(prep.P.z,prep.G.z);
+	[result.z] = optimize_voxel_error(prep.P.z,prep.G.z);
 
 
 	%% 2D Rand error
@@ -24,19 +24,19 @@ function assess_affinity_graph_script( fname, data, filtrad )
 	prob  = prep.P.xy;
 	truth = prep.G.xy;
 	disp(['Processing xy-affinity...']);
-	[data.xy] = optimize_2D_Rand_error(prob,truth);
+	[result.xy] = optimize_2D_Rand_error(prob,truth);
 
 	% yz-plane
 	prob  = rot90_3D(prep.P.yz,2,1);
 	truth = rot90_3D(prep.G.yz,2,1);
 	disp(['Processing yz-affinity...']);
-	[data.yz] = optimize_2D_Rand_error(prob,truth);
+	[result.yz] = optimize_2D_Rand_error(prob,truth);
 
 	% zx-plane
 	prob  = rot90_3D(prep.P.zx,1,3);
 	truth = rot90_3D(prep.G.zx,1,3);
 	disp(['Processing zx-affinity...']);
-	[data.zx] = optimize_2D_Rand_error(prob,truth);
+	[result.zx] = optimize_2D_Rand_error(prob,truth);
 
 
 	%% Save
@@ -44,6 +44,6 @@ function assess_affinity_graph_script( fname, data, filtrad )
 	if filtrad > 0
 		fname = [fname '.median' num2str(filtrad)];
 	end
-	save(fname,'data');
+	save(fname,'result');
 
 end
