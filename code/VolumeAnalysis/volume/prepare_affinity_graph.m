91cd62a2be622012911a7feea3444d51f46d2700
torms3 <kisuklee@mit.edu>
2015-01-13 11:22:13 -0500

    push

--- code/VolumeAnalysis/volume/prepare_affinity_graph.m
+++ code/VolumeAnalysis/volume/prepare_affinity_graph.m
@@ -23,11 +23,11 @@ function [prep] = prepare_affinity_graph( fname, data, w, filtrad )
 		P.x = adjust_border_effect(mvol{1},size(mvol{1})-w+one,true);
 		P.y = adjust_border_effect(mvol{2},size(mvol{2})-w+one,true);
 		P.z = adjust_border_effect(mvol{3},size(mvol{3})-w+one,true);
-	else
-		P.x = mvol{1}(2:end,2:end,2:end);
-		P.y = mvol{2}(2:end,2:end,2:end);
-		P.z = mvol{3}(2:end,2:end,2:end);
 	end
+	
+	P.x = mvol{1}(2:end,2:end,2:end);
+	P.y = mvol{2}(2:end,2:end,2:end);
+	P.z = mvol{3}(2:end,2:end,2:end);
 
 	P.xy = min(P.x,P.y);
 	P.yz = min(P.y,P.z);
