3362c573f9468138720ce4cc16b59498cf32642f
Kisuk Lee <torms3@gmail.com>
2015-01-22 16:12:39 -0500

    push

--- code/load_7nmDeeper.m
+++ code/load_7nmDeeper.m
@@ -1,12 +1,12 @@
 addpath_recurse
-homeDir = get_project_root_path();
+dataDir = get_project_data_path();
 
 % 7nmDeeper1
 disp('load 7nmDeeper1...');
-data{1} = double(loadtiff('7nm-415x415x577.tif'));
+data{1} = double(loadtiff([dataDir '/Ashwin/7nmDeeper/7nm-415x415x577.tif']));
 
 % 7nmDeeper2
 disp('load 7nmDeeper2...');
-data{2} = double(loadtiff('7nm-916x916x253.tif'));
+data{2} = double(loadtiff([dataDir '/Ashwin/7nmDeeper/7nm-916x916x253.tif']));
 
-clear homeDir;
+clear dataDir;
