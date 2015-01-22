57e7582fb3a06c06c201d7a575246a0db9a11e36
torms3 <kisuklee@mit.edu>
2015-01-21 20:17:50 -0500

    push

--- code/VolumeAnalysis/prepare_omnification_script.m
+++ code/VolumeAnalysis/prepare_omnification_script.m
@@ -12,18 +12,25 @@ function prepare_omnification_script( fname, w, filtrad, params, data )
 		fname = {fname};
 	end
 
-	if ~iscell(data)
-		data = {data};
+	if exist('data','var')
+		if ~iscell(data)
+			data = {data};
+		end
+	end
+
+	if ~exist('watershed','dir')
+		mkdir('watershed');
 	end
 
 	for i = 1:numel(fname)
 		prep = prepare_affinity_graph(fname{i},w,filtrad);
-		watershed(fname{i},prep.affin,params);
+		watershed(['watershed/' fname{i}],prep.affin,params);
+
 		if exist('data','var')
 			offset = floor(w/2) + [1 1 1];
 			sz = size(prep.P.x);
 			generate_channel(fname{i},data{i},offset,sz);
-		end
+		end		
 	end
 
 end
