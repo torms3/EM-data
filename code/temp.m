e89e0b1df9c4d64dd7c02e2014430a286fd9e53a
torms3 <kisuklee@mit.edu>
2015-03-04 06:38:38 -0500

    update

--- code/temp.m
+++ code/temp.m
@@ -6,37 +6,38 @@ end
 disp(fname);
 
 % Standard 1M
-% cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/iter_1M/output');
-% assess_affinity_graph_script(fname([5,11,12]),batch([5,11,12]),[],[37 37 37],0,[0 0 1]);
+% rng = 1:3;
+% cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/iter_1M/output');pwd
+% assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1 0]);
 
 % Standard 3.5M
-% cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/iter_3.5M/output');
-% assess_affinity_graph_script(fname([5,11,12]),batch([5,11,12]),[],[37 37 37],0,[0 0 1]);
+% cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/iter_3.5M/output');pwd
+% assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1 0]);
 
 % MALIS/exp1 500K
 rng = 1:3;
 cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp1/iter_500K/output');pwd
-assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
+assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1 0]);
 
 % MALIS/exp1 3M
 cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp1/iter_3M/output');pwd
-assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
+assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1 0]);
 
 % MALIS/exp1 4.5M
 rng = [1:3 5:12];
 cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp1/iter_3M/output');pwd
-assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
+assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1 0]);
 
 % MALIS/exp2 500K
 rng = 1:3;
 cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp2/iter_500K/output');pwd
-assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
+assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1 0]);
 
 % MALIS/exp2 3M
 cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp2/iter_3M/output');pwd
-assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
+assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1 0]);
 
 % MALIS/exp2 4.5M
 rng = [1:3 5:12];
 cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp2/iter_3M/output');pwd
-assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
+assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1 0]);
