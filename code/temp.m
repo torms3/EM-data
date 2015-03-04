7a0a5a872551354de20bfe637240994c90712160
torms3 <kisuklee@mit.edu>
2015-03-04 06:37:10 -0500

    update

--- code/temp.m
+++ code/temp.m
@@ -15,28 +15,28 @@ disp(fname);
 
 % MALIS/exp1 500K
 rng = 1:3;
-cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp1/iter_500K/output');
+cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp1/iter_500K/output');pwd
 assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
 
 % MALIS/exp1 3M
-cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp1/iter_3M/output');
+cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp1/iter_3M/output');pwd
 assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
 
 % MALIS/exp1 4.5M
 rng = [1:3 5:12];
-cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp1/iter_3M/output');
+cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp1/iter_3M/output');pwd
 assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
 
 % MALIS/exp2 500K
 rng = 1:3;
-cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp2/iter_500K/output');
+cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp2/iter_500K/output');pwd
 assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
 
 % MALIS/exp2 3M
-cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp2/iter_3M/output');
+cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp2/iter_3M/output');pwd
 assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
 
 % MALIS/exp2 4.5M
 rng = [1:3 5:12];
-cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp2/iter_3M/output');
+cd('~/Workbench/torms3/znn-release/experiments/e2198_e2006/SriniNet/exp2/malis/exp2/iter_3M/output');pwd
 assess_affinity_graph_script(fname(rng),batch(rng),[],[37 37 37],0,[0 0 1]);
