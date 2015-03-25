eb54c3a6ac457ef693a335adf5192130c22613c7
torms3 <kisuklee@mit.edu>
2015-03-24 20:41:34 -0400

    update

--- code/VolumeAnalysis/error/temp_prec_rec_curve.m
+++ code/VolumeAnalysis/error/temp_prec_rec_curve.m
@@ -18,18 +18,18 @@ function temp_prec_rec_curve(batch)
     standard{2} = result.xyz;
     load([header 'iter_5M/output/out' num2str(batch) tail]);                % standard 5M
     standard{3} = result.xyz;
-    load([header 'malis/exp1/iter_500K/output/out' num2str(batch) tail]);   % malis/exp1 1M
-    malis{1} = result.xyz;
-    load([header 'malis/exp1/iter_3M/output/out' num2str(batch) tail]);     % malis/exp1 3.5M
-    malis{2} = result.xyz;
-    load([header 'malis/exp1/iter_4.5M/output/out' num2str(batch) tail]);   % malis/exp1 5M
-    malis{3} = result.xyz;
-    % load([header 'malis/exp2/iter_500K/output/out' num2str(batch) tail]);   % malis/exp2 1M
+    % load([header 'malis/exp1/iter_500K/output/out' num2str(batch) tail]);   % malis/exp1 1M
     % malis{1} = result.xyz;
-    % load([header 'malis/exp2/iter_3M/output/out' num2str(batch) tail]);     % malis/exp2 3.5M
+    % load([header 'malis/exp1/iter_3M/output/out' num2str(batch) tail]);     % malis/exp1 3.5M
     % malis{2} = result.xyz;
-    % load([header 'malis/exp2/iter_4.5M/output/out' num2str(batch) tail]);     % malis/exp2 5M
+    % load([header 'malis/exp1/iter_4.5M/output/out' num2str(batch) tail]);   % malis/exp1 5M
     % malis{3} = result.xyz;
+    load([header 'malis/exp2/iter_500K/output/out' num2str(batch) tail]);   % malis/exp2 1M
+    malis{1} = result.xyz;
+    load([header 'malis/exp2/iter_3M/output/out' num2str(batch) tail]);     % malis/exp2 3.5M
+    malis{2} = result.xyz;
+    load([header 'malis/exp2/iter_4.5M/output/out' num2str(batch) tail]);     % malis/exp2 5M
+    malis{3} = result.xyz;
 
     header = '~/Workbench/torms3/znn-release/experiments/e2198_e2006/MALIS/output/original/';
     load([header 'out' num2str(batch) tail]);     % EyeWire
