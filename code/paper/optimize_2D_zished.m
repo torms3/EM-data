ae96d3dbbdffe577199c2f00f286e507c5d8b2b4
torms3 <kisuklee@mit.edu>
2015-05-25 17:43:29 -0400

    update

--- code/paper/optimize_2D_zished.m
+++ code/paper/optimize_2D_zished.m
@@ -3,12 +3,12 @@ function optimize_2D_zished( ipath, gpath )
     % zished path
     zished = '/data/home/kisuklee/Workbench/seung-lab/watershed/zi/watershed/bin/watershed';
 
-    high = 0.99;
-    low  = 0.3;
+    high = 999;
+    low  = 300;
     sz   = 25;
-    thld = 0.4;
+    thld = 400;
 
-    for high = 0.5:0.1:1
+    for high = 500:100:1000
         disp(['high = ' num2str(high)]);
         run_zished;
     end
@@ -17,7 +17,7 @@ function optimize_2D_zished( ipath, gpath )
 
         % arguments
         args = sprintf(' --ipath=%s --gpath=%s --high %.3f --low %.3f --size %d --thold %.3f', ...
-                       ipath,gpath,high/1000,low/1000,sz,thold/1000);
+                       ipath,gpath,high/1000,low/1000,sz,thld/1000);
         sysline = [zished args];
         disp(sysline);
         [~,cmdout] = system(sysline);
