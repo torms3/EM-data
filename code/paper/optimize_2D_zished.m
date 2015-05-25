7b985ecde1158dea10ae7f45a955c1c1086e4fca
torms3 <kisuklee@mit.edu>
2015-05-25 18:20:43 -0400

    update

--- code/paper/optimize_2D_zished.m
+++ code/paper/optimize_2D_zished.m
@@ -19,7 +19,7 @@ function ret = optimize_2D_zished( ipath, gpath )
     function ret = iterate_over(thresh)
         for i = 1:numel(thresh)
             data = run_zished(thresh(i),low,sz,thld);
-            prec(i) = data(1);
+            prec(i) = data{1};
             rec(i) = data{2};
             re(i) = data{3};
         end
