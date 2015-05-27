acee4789469997715bf8c446fc1e2435fd986294
torms3 <kisuklee@mit.edu>
2015-05-27 06:48:33 -0400

    update

--- code/paper/vol2tif_script.m
+++ code/paper/vol2tif_script.m
@@ -2,7 +2,11 @@ function vol2tif_script( key )
 
     list = dir(key);
     for i = 1:numel(list)
-        vol2tif(list(i).name);
+        fname = list(i).name;
+        if any(findstr(fname,'.size'))
+            continue;
+        end
+        vol2tif(fname);
     end
 
 end
