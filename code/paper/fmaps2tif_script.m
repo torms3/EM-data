67bf5b887ba4e851e2012b0fcb69e8245960f45e
torms3 <kisuklee@mit.edu>
2015-05-27 03:47:23 -0400

    update

---
+++ code/paper/fmaps2tif_script.m
@@ -0,0 +1,42 @@
+function fmaps2tif_script( net_type, fpath )
+
+    if ~exist('net_type','var'); net_type = '2D';end;
+    if ~exist('fpath','var');        fpath = pwd;end;
+    cd(fpath);
+
+    keys = {'INPUT','C1','C2','C3','FC','OUTPUT'};
+    switch net_type
+    case '2D'
+        z = [1 1 1 1 1 1];
+        layer = containers.Map(keys,z);
+    case '3D'
+        z = [2 2 2 2 1 1];
+        layer = containers.Map(keys,z);
+    case '2D-R'
+        z = [1 1 1 1 1 1];
+        layer = containers.Map(keys,z);
+        layer('INPUT2') = 1;
+    case '3D-R'
+        z = [2 2 2 2 1 1];
+        layer = containers.Map(keys,z);
+        layer('INPUT2') = 2;
+    otherwise
+        assert(false);
+    end
+
+    list = dir('out*');
+    for i = 1:numel(list)
+
+        fname = list(i).name;
+        if any(findstr(fname,'size'))
+            continue;
+        end
+
+        idx = findstr(fname,'.');
+        z = layer(fname(idx:end));
+        disp([fname(idx:end) ' = ' num2str(z)]);        
+        % fmaps2tif(fname,[],z);
+
+    end
+
+end
--- code/train_info/monitor_learning.m
+++ code/train_info/monitor_learning.m
@@ -1,15 +1,16 @@
 function [data] = monitor_learning( cost_type, avg_winidow, start_iter, errshade, errline )
 
 	fontsize = get(0,'DefaultAxesFontSize');
-  set(0,'DefaultAxesFontSize',12);
+ 	set(0,'DefaultAxesFontSize',12);
 
 	%% Options
 	%
 	if ~exist('cost_type','var'); 	cost_type = 'Cost';end;
-	if ~exist('avg_winidow','var'); 	 avg_winidow = 0;end;
+	if ~exist('avg_winidow','var');    avg_winidow = 0;end;
 	if ~exist('start_iter','var'); 	start_iter = [1 1];end;
-	if ~exist('errshade','var');			errshade = [0 0];end;
-	if ~exist('errline','var');				 errline = false;end;
+	if ~exist('errshade','var');	  errshade = [0 0];end;
+	if ~exist('errline','var');		   errline = false;end;
+	train_only = true;
 
 	% Load train info
 	[train] = load_info('train');
@@ -66,24 +67,28 @@ function [data] = monitor_learning( cost_type, avg_winidow, start_iter, errshade
 
 		% train		
 		h(1) = plot(train.iter,train.err,'-k');
+		lgnd{1} = 'Train';
 		% xlim([train.iter(1) train.iter(end)]);
 		if isfield(train,'stderr') & errshade(1)
 			stderr = [train.stderr(:) train.stderr(:)];
 			shadedErrorBar(train.iter,train.err,stderr,{'-k','LineWidth',1.5},1,errline);
 		end
 
-		% test		
-		h(2) = plot(test.iter,test.err,'-r');
-		if isfield(test,'stderr') & errshade(2)
-			stderr = [test.stderr(:) test.stderr(:)];
-			shadedErrorBar(test.iter,test.err,stderr,{'-r','LineWidth',1.5},1,errline);
+		% test
+		if ~train_only
+			h(2) = plot(test.iter,test.err,'-r');
+			lgnd{2} = 'Test';
+			if isfield(test,'stderr') & errshade(2)
+				stderr = [test.stderr(:) test.stderr(:)];
+				shadedErrorBar(test.iter,test.err,stderr,{'-r','LineWidth',1.5},1,errline);
+			end
 		end
 					
 		% axis([0 max(train.iter) 0 max(train.err)]);
 		xlabel('Iteration');
 		ylabel(cost_type);
 		title(['Cost' avgStr]);
-		legend(h,'Train','Test');
+		legend(h,lgnd);
 
 	hold off;
 	grid on;
@@ -94,6 +99,7 @@ function [data] = monitor_learning( cost_type, avg_winidow, start_iter, errshade
 
 		% train
 		h(1) = plot(train.iter,train.cls,'-k');
+		lgnd{1} = 'Train';
 		% xlim([train.iter(1) train.iter(end)]);
 		if isfield(train,'stdcls') & errshade(1)
 			stdcls = [train.stdcls(:) train.stdcls(:)];
@@ -101,17 +107,20 @@ function [data] = monitor_learning( cost_type, avg_winidow, start_iter, errshade
 		end
 
 		% test
-		h(2) = plot(test.iter,test.cls,'-r');
-		if isfield(train,'stdcls') & errshade(2)
-			stdcls = [test.stdcls(:) test.stdcls(:)];
-			shadedErrorBar(test.iter,test.cls,stdcls,{'-r','LineWidth',1.5},1,errline);
+		if ~train_only
+			h(2) = plot(test.iter,test.cls,'-r');
+			lgnd{2} = 'Test';
+			if isfield(train,'stdcls') & errshade(2)
+				stdcls = [test.stdcls(:) test.stdcls(:)];
+				shadedErrorBar(test.iter,test.cls,stdcls,{'-r','LineWidth',1.5},1,errline);
+			end
 		end
 
 		% axis([0 max(train.iter) 0 max(train.err)]);
 		xlabel('Iteration');
 		ylabel('Classification error');
 		title(['Classification Error' avgStr]);
-		legend(h,'Train','Test');
+		legend(h,lgnd);
 
 	hold off;
 	grid on;
