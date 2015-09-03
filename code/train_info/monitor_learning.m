b179dfe1ff1bb6ea00d979a5438a082a93895833
kisuklee <kisuklee@nat-oitwireless-inside-vapornet100-a-24210.Princeton.EDU>
2015-09-03 18:19:53 -0400

    smoothing modified

--- code/train_info/monitor_learning.m
+++ code/train_info/monitor_learning.m
@@ -1,4 +1,4 @@
-function [data] = monitor_learning( cost_type, avg_winidow, start_iter, errshade, errline )
+function [data] = monitor_learning( method, avg_winidow, start_iter, errshade, errline )
 
 	train_only = false;
 	% train_only = true;
@@ -8,7 +8,7 @@ function [data] = monitor_learning( cost_type, avg_winidow, start_iter, errshade
 
 	%% Options
 	%
-	if ~exist('cost_type','var'); 	cost_type = 'Cost';end;
+	if ~exist('method','var');		 method = 'moving';end;
 	if ~exist('avg_winidow','var');    avg_winidow = 0;end;
 	if ~exist('start_iter','var'); 	start_iter = [1 1];end;
 	if ~exist('errshade','var');	  errshade = [0 0];end;
@@ -30,38 +30,38 @@ function [data] = monitor_learning( cost_type, avg_winidow, start_iter, errshade
 	test.err(idx) 	= [];
 	test.cls(idx) 	= [];
 
-	if strcmp(cost_type,'RMSE')
-		train.err = sqrt(train.err);
-		test.err  = sqrt(test.err);
-	end
+	% if strcmp(cost_type,'RMSE')
+	% 	train.err = sqrt(train.err);
+	% 	test.err  = sqrt(test.err);
+	% end
 
 	% train/test checkpoint frequency ratio
 	ratio = train.n/test.n;
 
-	% windowing
-	train.iter 	= train.iter(start_iter(1):end);
-	train.err 	= train.err(start_iter(1):end);
-	train.cls 	= train.cls(start_iter(1):end);
-	train.n 	= numel(train.iter);
-	test.iter 	= test.iter(start_iter(2):end);
-	test.err 	= test.err(start_iter(2):end);
-	test.cls 	= test.cls(start_iter(2):end);
-	test.n 		= numel(test.iter);
-
-	% convolution filter
-	[train] = smooth_curve(train,avg_winidow);
-	[test] 	= smooth_curve(test,avg_winidow);
-	% [test] 	= smooth_curve(test,floor(avg_winidow/ratio));
+	% smoothing
+	[train] = smooth_curve(train,avg_winidow,method);
+	% [test] 	= smooth_curve(test,avg_winidow,method);
+	[test] 	= smooth_curve(test,floor(avg_winidow/ratio),method	);
 	if( avg_winidow > 0 )
 		avgStr = [', Smoothing Window = ' num2str(avg_winidow)];
 	else
 		avgStr = '';
 	end
 
+	% windowing
+	% train.iter 	= train.iter(start_iter(1):end);
+	% train.err 	= train.err(start_iter(1):end);
+	% train.cls 	= train.cls(start_iter(1):end);
+	% train.n 	= numel(train.iter);
+	% test.iter 	= test.iter(start_iter(2):end);
+	% test.err 	= test.err(start_iter(2):end);
+	% test.cls 	= test.cls(start_iter(2):end);
+	% test.n 		= numel(test.iter);
+
 	% return data
 	data.train = train;
 	data.test  = test;
-	data.cost  = cost_type;
+	% data.cost  = cost_type;
 
 	% Plot cost
 	figure;
@@ -86,9 +86,10 @@ function [data] = monitor_learning( cost_type, avg_winidow, start_iter, errshade
 			end
 		end
 					
-		% axis([0 max(train.iter) 0 max(train.err)]);
+		xl = xlim;
+		xlim([train.iter(start_iter(1)) xl(2)]);
 		xlabel('Iteration');
-		ylabel(cost_type);
+		ylabel('Cost');
 		title(['Cost' avgStr]);
 		legend(h,lgnd);
 
@@ -118,7 +119,8 @@ function [data] = monitor_learning( cost_type, avg_winidow, start_iter, errshade
 			end
 		end
 
-		% axis([0 max(train.iter) 0 max(train.err)]);
+		xl = xlim;
+		xlim([train.iter(start_iter(1)) xl(2)]);
 		xlabel('Iteration');
 		ylabel('Classification error');
 		title(['Classification Error' avgStr]);
