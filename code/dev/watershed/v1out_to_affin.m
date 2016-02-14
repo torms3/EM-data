358474909d4d2c36184d3a9baa9648a4d0dbef36
torms3 <kisuklee@mit.edu>
2016-02-14 18:16:20 -0500

    update

--- code/dev/watershed/v1out_to_affin.m
+++ code/dev/watershed/v1out_to_affin.m
@@ -1,17 +1,25 @@
-function v1out_to_affin( fname, newname )
+function v1out_to_affin( fname, oname )
 
-    % e.g. fname    = 'out103'
-    %      newname  = 'Piriform_sample9_output'
-    aff{1} = import_volume([fname '.0'],[],[],'double');
-    aff{2} = import_volume([fname '.1'],[],[],'double');
-    aff{3} = import_volume([fname '.2'],[],[],'double');
+    if ~iscell(fname); fname = {fname}; end;
+    if ~iscell(oname); oname = {oname}; end;
+    assert(numel(fname)==numel(oname));
 
-    aff = single(cat(4,aff{:}));
+    for i = 1:numel(fname)
 
-    % export affinity graph binary
-    export_tensor(newname,aff,'affin','single');
+        % e.g. fname = 'out103'
+        %      oname = 'Piriform_sample9_output'
+        aff{1} = import_volume([fname{i} '.0'],[],[],'double');
+        aff{2} = import_volume([fname{i} '.1'],[],[],'double');
+        aff{3} = import_volume([fname{i} '.2'],[],[],'double');
 
-    % exprot hdf5
-    % hdf5write([fname '.h5'],'/main',aff);
+        aff = single(cat(4,aff{:}));
+
+        % export affinity graph binary
+        export_tensor(oname{i},aff,'affin','single');
+
+        % exprot hdf5
+        % hdf5write([oname{i} '.h5'],'/main',aff);
+
+    end
 
 end
