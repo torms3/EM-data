5df03529c3a1ce8744023ee8b6ea150ead77f033
torms3 <kisuklee@mit.edu>
2016-04-19 04:04:20 -0400

    update

--- code/papers/multiscale/mean_affinity_script.m
+++ code/papers/multiscale/mean_affinity_script.m
@@ -11,7 +11,8 @@ function mean_affinity_script()
         for j = 1:numel(samples)
             sample = samples(j);
             iname  = [pwd '/' sprintf(template,sample) '.h5'];
-            oname  = [pwd '/' sprintf(template,sample) '_mean_affinity'];
+            % oname  = [pwd '/' sprintf(template,sample) '_mean_affinity'];
+            oname  = [pwd '/' sprintf(template,sample) '_mean_affinity_low0'];
             if exist([oname '.h5'], 'file') ~= 2
                 disp(['mean affinity agglomeration: ' oname '.h5']);
                 mean_affinity_agglomeration(iname, [oname '.h5']);
