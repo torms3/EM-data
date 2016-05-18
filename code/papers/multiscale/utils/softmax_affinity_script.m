cf6f6e19dae269ad29f46832612a9a3a989c8d44
torms3 <kisuklee@mit.edu>
2016-05-18 03:21:19 -0400

    update

--- code/papers/multiscale/utils/softmax_affinity_script.m
+++ code/papers/multiscale/utils/softmax_affinity_script.m
@@ -7,24 +7,32 @@ function softmax_affinity_script( prefix, samples, replace_tif )
         sample = samples(i);
 
         % construct file name
-        fname  = [prefix '_sample' num2str(sample) '_output'];
+        fname  = [prefix '_sample' num2str(sample)];
+        oname  = [fname '_output'];
 
         % apply softmax to get affinity
         [affs] = softmax_affinity(prefix,sample);
         [affs] = flip(affs,4);
 
         % export affinity graph binary
-        export_tensor(fname,affs,'affin','single');
+        export_tensor(oname,affs,'affin','single');
 
         % exprot hdf5
-        hdf5write([fname '.h5'],'/main',affs);
+        hdf5write([oname '.h5'],'/main',affs);
 
         if replace_tif
-            delete([fname '*.tif']);
-            saveastiff(single(affs(:,:,:,3)),[fname '_0.tif']); % z-affinity
-            saveastiff(single(affs(:,:,:,2)),[fname '_1.tif']); % y-affinity
-            saveastiff(single(affs(:,:,:,1)),[fname '_2.tif']); % x-affinity
+            delete([oname '*.tif']);
+        else
+            for i = 1:6
+                src = [oname '_' num2str(i-1) '.tif'];
+                dst = [fname '_score_' num2str(i-1) '.tif'];
+                movefile(src,dst);
+            end
         end
+
+        saveastiff(single(affs(:,:,:,3)),[oname '_0.tif']); % z-affinity
+        saveastiff(single(affs(:,:,:,2)),[oname '_1.tif']); % y-affinity
+        saveastiff(single(affs(:,:,:,1)),[oname '_2.tif']); % x-affinity
 
     end
 
