f6c3fc605448e12e8862640ed254a2552a2df4de
torms3 <kisuklee@mit.edu>
2016-05-20 21:36:33 -0400

    load Piriform data from local disk

--- code/dev/measure/load_Piriform_dataset.m
+++ code/dev/measure/load_Piriform_dataset.m
@@ -4,16 +4,14 @@ function data = load_Piriform_dataset( idx )
     if isempty(idx);        idx = 1:10; end;
 
     cur = pwd;
-    cd(get_project_data_path);
+    cd('~/Data_local/datasets/Piriform/');
 
-        cd('Piriform');
-
-        for i = 1:numel(idx)
-            n = idx(i);
-            disp(['load stack' num2str(n) '...']);
-            data{i}.image = loadtiff(['stack' num2str(n) '-image.tif']);
-            data{i}.label = loadtiff(['stack' num2str(n) '-label.tif']);
-        end
+    for i = 1:numel(idx)
+        n = idx(i);
+        disp(['load stack' num2str(n) '...']);
+        data{i}.image = loadtiff(['stack' num2str(n) '-image.tif']);
+        data{i}.label = loadtiff(['stack' num2str(n) '-label.tif']);
+    end
 
     cd(cur);
 
