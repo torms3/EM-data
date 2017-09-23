5ad9f5d5d6435678fc4bdad4ad7bd6146eb7b893
torms3 <kisuklee@mit.edu>
2017-09-23 02:30:43 -0400

    h5_to_tif fixed

--- code/dev/watershed/h5_to_tif.m
+++ code/dev/watershed/h5_to_tif.m
@@ -1,11 +1,21 @@
 function h5_to_tif( fname, dataset, idx )
 
-    if ~exist('dataset','var'); dataset = 'main'; end;
+    if ~exist('dataset','var')
+        dataset = '/main';
+    end
 
-    out = hdf5read([fname '.h5'], ['/' dataset]);
+    % Read HDF5 file.
+    [fpath,fname,ext] = fileparts(fname);
+    out = hdf5read([fname ext], dataset);
 
-    for i = 1:size(out,4)
-        saveastiff(out(:,:,:,i), [fname '_' num2str(i-1) '.tif'] );
+    % Save as tiff.
+    dim = size(out,4);
+    if dim == 1
+        saveastiff(out, [fname '.tif']);
+    else
+        for i = 1:dim
+            saveastiff(out(:,:,:,i), [fname '_' num2str(i-1) '.tif'] );
+        end
     end
 
 end
