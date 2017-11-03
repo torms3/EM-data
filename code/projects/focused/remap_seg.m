0b5803f193727d3e1befd8c4923da616ee87fe33
torms3 <kisuklee@mit.edu>
2017-11-03 01:25:13 -0400

    update

--- code/projects/focused/remap_seg.m
+++ code/projects/focused/remap_seg.m
@@ -1,5 +1,7 @@
 function [seg] = remap_seg( seg, bdr )
 
+    if ~exist('bdr','var'); bdr = 1; end;
+
     [C,~,ic] = unique(seg);
     remap = (1:numel(C)) - bdr;
     seg = reshape(remap(ic),size(seg));
